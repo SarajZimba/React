02b874cc182aa134b35851f5285a2d65c7bc946e
SarajZimba <sarajzimba@gmail.com>
2023-06-21 12:17:44 +0300

    bootstrap used

--- src/Myroutes.jsx
+++ src/Myroutes.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import First from './First'
 import Second from './Second'
+import Layout from './components/layout/Layout'
 
 const Myroutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<First/>}/>
-        <Route path='/second' element={<Second/>}/>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<First/>}/>
+          <Route path='/second' element={<Second/>}/>
+        </Route>
       </Routes>
     
     </BrowserRouter>
